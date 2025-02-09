1ddf2b199d2d582d408fdc8e254f5f9c468bae01
abhishek gautam <iamdeveloperabhishek@gmail.com>
2025-02-10 00:35:18 +0530

    changes

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
-const Pagination = ({ totalPages, setPage }) => {
+const Pagination = ({ totalPages, currentPage, setPage }) => {
   const dispatch = useDispatch();
+
   return (
-    <div className="flex space-x-2 mt-6">
+    <div className="flex items-center space-x-2 mt-6">
+    
       {Array.from({ length: totalPages }, (_, i) => (
-        <button key={i} className="px-4 py-2 bg-gray-300 rounded-lg shadow-md" onClick={() => dispatch(setPage(i + 1))}>{i + 1}</button>
+        <button
+          key={i}
+          className={`px-4 py-2 rounded-full shadow-md ${
+            currentPage === i + 1 ? "bg-black text-white font-bold" : "bg-gray-300 hover:bg-gray-400"
+          }`}
+          onClick={() => dispatch(setPage(i + 1))}
+        >
+          {i + 1}
+        </button>
       ))}
     </div>
   );
